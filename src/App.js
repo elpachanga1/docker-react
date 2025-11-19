20ca4c5b4753a8ed4098c918210d07d262688f15
Fabian Guerrero Montaño <fguerrero@remarquesystems.com>
2025-11-19 15:48:50 -0500

    test for automated pipeline upload

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          holiii
+          holiii Papi esto si sube?
         </p>
         <a
           className="App-link"
